9a28aa3d8fa8c73d6e4ee33b262f2499f2107d60
Lenvia <yrp1199@163.com>
2022-05-12 16:11:22 +0800

    Update: revise svm

--- svm_based/src/svm_classifier.py
+++ svm_based/src/svm_classifier.py
@@ -11,6 +11,10 @@ import os
 
 n_dim = 200
 
+svm_data_dir = '../svm_data'
+
+if not os.path.exists(svm_data_dir):
+    os.mkdir(svm_data_dir)
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -58,7 +62,8 @@ def build_sentence_vector(text, size, model):
     return vec
 
 
-def get_vecs(svm_data_dir, train_data, dev_data):
+# 为 train 和 dev 中的所有句子生成向量
+def get_vecs_from_words_list(train_data, dev_data):
     # 初始化模型和词表
     model = Word2Vec(train_data, min_count=10, vector_size=n_dim)
 
@@ -87,10 +92,10 @@ def get_vecs(svm_data_dir, train_data, dev_data):
 
     # dev_vecs = scale(dev_vecs)
     np.save(os.path.join(svm_data_dir, 'dev_vecs.npy'), dev_vecs)
-    # print(dev_vecs.shape)
+    
 
 
-def get_data(svm_data_dir):
+def load_saved_data():
     train_vecs = np.load(os.path.join(svm_data_dir, 'train_vecs.npy'))
     train_labels = np.load(os.path.join(svm_data_dir, 'train_labels.npy'))
     dev_vecs = np.load(os.path.join(svm_data_dir, 'dev_vecs.npy'))
@@ -98,24 +103,24 @@ def get_data(svm_data_dir):
     return train_vecs, train_labels, dev_vecs, dev_labels
 
 
-def svm_train(svm_data_dir, train_vecs, train_labels, dev_vecs, dev_labels):
+def svm_train(train_vecs, train_labels, dev_vecs, dev_labels):
     clf = SVC(C=0.5, kernel='rbf')
     clf.fit(train_vecs, train_labels)
     joblib.dump(clf, os.path.join(svm_data_dir, 'svm_model.pkl'))
     print(clf.score(dev_vecs, dev_labels))
 
 
-def get_predict_vecs(svm_data_dir, words):
+def get_predict_vecs(words):
     model = Word2Vec.load(os.path.join(svm_data_dir, 'w2v_model.pkl'))
     vecs = build_sentence_vector(words, n_dim, model)
-    # print(vecs.shape)
+
     return vecs
 
 
 # 对单个句子进行情感判断
-def svm_predict(svm_data_dir, string):
+def svm_predict(string):
     words = jieba.lcut(string)
-    words_vecs = get_predict_vecs(svm_data_dir, words).reshape(1, -1)
+    words_vecs = get_predict_vecs(words).reshape(1, -1)
 
     # print(words_vecs)
     clf = joblib.load(os.path.join(svm_data_dir, 'svm_model.pkl'))
@@ -129,30 +134,25 @@ if __name__ == '__main__':
     parser.add_argument('--is_train', type=str2bool, default=False, help='mode')
     parser.add_argument('--train_path', type=str, default='../../dataset/ChnSentiCorp/train.tsv')
     parser.add_argument('--dev_path', type=str, default='../../dataset/ChnSentiCorp/dev.tsv')
-    parser.add_argument('--svm_data_dir', type=str, default='../svm_data')
     parser.add_argument('--svm_model', type=str, default='svm_model.pkl', help='save model name')
 
     args = parser.parse_args()  # 解析参数
 
-    if not os.path.exists(args.svm_data_dir):
-        os.mkdir(args.svm_data_dir)
-
-    # print(args.is_train)
     if args.is_train is True:
         train_labels, train_sents, train_words_list = read_data(args.train_path, isTrain=True)
         dev_labels, dev_sents, dev_words_list = read_data(args.dev_path)
 
-        np.save(os.path.join(args.svm_data_dir, 'train_labels.npy'), train_labels)
-        np.save(os.path.join(args.svm_data_dir, 'dev_labels.npy'), dev_labels)
+        np.save(os.path.join(svm_data_dir, 'train_labels.npy'), train_labels)
+        np.save(os.path.join(svm_data_dir, 'dev_labels.npy'), dev_labels)
 
-        get_vecs(args.svm_data_dir, train_words_list, dev_words_list)
+        get_vecs_from_words_list(train_words_list, dev_words_list)
 
-        train_vecs, train_labels, dev_vecs, dev_labels = get_data(args.svm_data_dir)
+        train_vecs, train_labels, dev_vecs, dev_labels = load_saved_data()
 
-        svm_train(args.svm_data_dir, train_vecs, train_labels, dev_vecs, dev_labels)
-        # pass
+        svm_train(train_vecs, train_labels, dev_vecs, dev_labels)
+
 
     else:
         text = "在当当上买了很多书，都懒于评论。但这套书真的很好，3册都非常精彩。我家小一的女儿，认字多，非常喜爱，每天睡前必读。她还告诉我，学校的语文课本中也有相同的文章。我还借给我的同事的女儿，我同事一直头疼她女儿不爱看书，但这套书，她女儿非常喜欢。两周就看完了。建议买。很少写评论，但忍不住为这套书写下。也给别的读者参考下。"
-        res = svm_predict(args.svm_data_dir, text)
+        res = svm_predict(text)
         print(res)
